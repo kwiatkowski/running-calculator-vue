07edce354ba5af214996aedb8fcf6318981b2536
Marcin Kwiatkowski <kwiatkowski.work@gmail.com>
2024-03-02 14:54:54 +0000

    prod

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  publicPath: '/training/',
+  publicPath: '/training/web/',
   lintOnSave: false,
   outputDir: 'web',
   devServer: {
